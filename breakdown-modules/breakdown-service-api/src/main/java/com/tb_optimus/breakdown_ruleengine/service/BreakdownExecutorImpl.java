f0666f770d6842c518ba4c4c8025de90dba7fcf7
Taras Bulubas <t.bulubas@tb-optimus.com>
2017-02-19 16:48:02 +0100

    making spring service with spring boot

--- breakdown-modules/breakdown-service-api/src/main/java/com/tb_optimus/breakdown_ruleengine/service/BreakdownExecutorImpl.java
+++ breakdown-modules/breakdown-service-api/src/main/java/com/tb_optimus/breakdown_ruleengine/service/BreakdownExecutorImpl.java
@@ -1,7 +1,9 @@
 package com.tb_optimus.breakdown_ruleengine.service;
 
 import com.tb_optimus.breakdown.Order;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BreakdownExecutorImpl implements BreakdownExecutor {
 
     public Boolean executeBreakdown(Order order) {
--- breakdown-modules/breakdown-service-impl/src/main/java/com/tb_optimus/breakdown_ruleengine/service/BreakdownExecutor.java
+++ breakdown-modules/breakdown-service-impl/src/main/java/com/tb_optimus/breakdown_ruleengine/service/BreakdownExecutor.java
@@ -1,7 +1,9 @@
 package com.tb_optimus.breakdown_ruleengine.service;
 
 import com.tb_optimus.breakdown.Order;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface BreakdownExecutor {
 
     Boolean executeBreakdown(Order order);
