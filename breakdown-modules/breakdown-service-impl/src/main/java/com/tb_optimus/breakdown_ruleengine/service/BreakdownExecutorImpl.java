55778c551819f712b5df7a4e4efadf03a6869b36
Taras Bulubas <t.bulubas@tb-optimus.com>
2017-02-27 03:30:19 +0100

    calling solver in service

--- breakdown-modules/breakdown-service-impl/src/main/java/com/tb_optimus/breakdown_ruleengine/service/BreakdownExecutorImpl.java
+++ breakdown-modules/breakdown-service-impl/src/main/java/com/tb_optimus/breakdown_ruleengine/service/BreakdownExecutorImpl.java
@@ -1,11 +1,16 @@
 package com.tb_optimus.breakdown_ruleengine.service;
 
 import com.tb_optimus.breakdown.ColourSizeOrder;
+import com.tb_optimus.breakdown_ruleengine.solver.services.api.BreakdownSolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BreakdownExecutorImpl implements BreakdownExecutor
 {
+
+    @Autowired
+    private BreakdownSolver breakdownSolver;
 
     public Boolean executeBreakdown(ColourSizeOrder order) {
         return null;
---
+++ breakdown-modules/breakdown-service-impl/src/test/java/com/tb_optimus/breakdown_ruleengine/service/BreakdownExecutorImplTest.java
@@ -0,0 +1,16 @@
+package com.tb_optimus.breakdown_ruleengine.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BreakdownExecutorImplTest {
+
+    @Test
+    public void shouldExecuteBreakdown() {
+        // given
+        // when
+        // then
+    }
+}
